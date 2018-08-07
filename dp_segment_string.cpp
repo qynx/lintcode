9f83b3ad3ce7cfbf7e617cae1500cd139934ced3
jiaqian <zslngu@163.com>
2018-08-07 11:54:30 +0800

    new

---
+++ dp_segment_string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+#include<bits\stdc++.h>
+using namespace std;
+
+/*
+
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s ="leetcode",
+dict =["leet", "code"].
+
+如果我们想知道某个子串是否可由dict中的几个单词拼接而成就可以用这样的方式得到结果
+（满足条件为True, 不满足条件为False）存入到一个boolean数组的对应位置上
+*/ 
+int main(){
+	unordered_set<string> dict;
+} 
